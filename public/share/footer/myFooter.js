b6008acadfa6865975bcc9f2f4dbbea61121fa61
awinder0230 <awinder0230@gmail.com>
2018-09-19 00:24:39 +0800

    share footer

---
+++ public/share/footer/myFooter.js
@@ -0,0 +1,33 @@
+document.getElementById("myFooter").innerHTML = 
+  '<div class="container">'+
+    '<div class="row">'+
+      '<div class="col-lg-5 col-md-6 col-sm-6">'+
+        '<div class="single-footer-widget">'+
+          '<h6>Footprint</h6>'+
+          '<p>'+
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.'+
+          '</p>'+
+          '<p class="footer-text">'+
+            'This template is modified from <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>'+
+          '</p>'+
+        '</div>'+
+      '</div>'+
+      '<div class="col-lg-5  col-md-6 col-sm-6">'+
+        '<div class="single-footer-widget">'+
+          '<h6>Newsletter</h6>'+
+          '<p>Stay update with our latest</p>'+
+        '</div>'+
+      '</div>'+
+      '<div class="col-lg-2 col-md-6 col-sm-6 social-widget">'+
+        '<div class="single-footer-widget">'+
+          '<h6>Follow Me</h6>'+
+          '<p>Let us be social</p>'+
+          '<div class="footer-social d-flex align-items-center">'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/tzu-sheng-kuo/"><i class="fa fa-linkedin"></i></a>'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://github.com/awinder0230"><i class="fa fa-github"></i></a>'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/JasonKuo0230"><i class="fa fa-twitter"></i></a>'+
+          '</div>'+
+        '</div>'+
+      '</div>'+
+    '</div>'+
+  '</div>';
