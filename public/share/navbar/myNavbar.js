d721091482c01c75056127346fcac095b56bc335
awinder0230 <awinder0230@gmail.com>
2018-09-18 20:21:49 +0800

    share navbar

---
+++ public/share/navbar/myNavbar.js
@@ -0,0 +1,16 @@
+document.getElementById("myNavbar").innerHTML = 
+  '<div class="container">'+
+    '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+      '<span class="text-white lnr lnr-menu"></span>'+
+    '</button>'+
+    '<div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">'+
+      '<ul class="navbar-nav">'+
+        '<li><a href="/">Home</a></li>'+
+        '<li><a href="/#about">About</a></li>'+
+        '<li><a href="/#portfolio">Portfolio</a></li>'+
+        '<li><a href="/#awards">Awards</a></li>'+
+        '<li><a href="/#activities">Activities</a></li>'+
+        '<li><a href="/public/assets/cv/cv-Kuo.pdf">Resume</a></li>'+
+      '</ul>'+
+    '</div>'+
+  '</div>';
