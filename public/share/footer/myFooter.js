7a4ca392f8b5793e487156d61cc30f828ff69f49
awinder0230 <user@USERde-MacBook-Air.local>
2018-11-24 02:23:59 +0800

    about and footer

--- public/share/footer/myFooter.js
+++ public/share/footer/myFooter.js
@@ -8,20 +8,19 @@ document.getElementById("myFooter").innerHTML =
             'something here'+
           '</p>'+
           '<p class="footer-text">'+
-            'This template is modified from <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>'+
+            'This template is modified from <a style="color: #555555;" href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>'+
           '</p>'+
         '</div>'+
       '</div>'+
       '<div class="col-lg-5  col-md-6 col-sm-6">'+
         '<div class="single-footer-widget">'+
-          '<h6>Newsletter</h6>'+
-          '<p>something here</p>'+
+          '<h6>Contact</h6>'+
+          '<p"><i class="fa fa-envelope"></i><a href="mailto:awinder0230@gmail.com" style="color: grey;"> awinder0230@gmail.com</a></p>'+
         '</div>'+
       '</div>'+
       '<div class="col-lg-2 col-md-6 col-sm-6 social-widget">'+
         '<div class="single-footer-widget">'+
-          '<h6>Follow Me</h6>'+
-          '<p>Let us be social</p>'+
+          '<h6>Social Media</h6>'+
           '<div class="footer-social d-flex align-items-center">'+
             '<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/tzu-sheng-kuo/"><i class="fa fa-linkedin"></i></a>'+
             '<a target="_blank" rel="noopener noreferrer" href="https://github.com/awinder0230"><i class="fa fa-github"></i></a>'+
