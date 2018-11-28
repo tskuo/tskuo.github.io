60fd195bb40182b3709644da4a6f4c08ae81f2d0
awinder0230 <user@USERde-MacBook-Air.local>
2018-11-28 19:23:49 +0800

    about and justify

--- public/share/navbar/myNavbar.js
+++ public/share/navbar/myNavbar.js
@@ -6,7 +6,7 @@ document.getElementById("myNavbar").innerHTML =
     '<div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">'+
       '<ul class="navbar-nav">'+
         '<li><a href="/">Home</a></li>'+
-        '<li><a href="/public/pages/about/">About</a></li>'+
+        '<li><a href="/#about">About</a></li>'+
         '<li><a href="/#portfolio">Portfolio</a></li>'+
         '<li><a href="/#awards">Awards</a></li>'+
         '<li><a href="/#activity">Leadership</a></li>'+
